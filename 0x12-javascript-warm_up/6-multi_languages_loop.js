2af0c4b44318fd83df6572602f93611ef4dc973c
phil-droid <akkoli058@gmail.com>
2023-03-15 06:53:39 -0700

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Uses an array to loop through and print strings
+
+const lines = ["C is fun', 'Python is cool', 'Javascript is amazing"];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
